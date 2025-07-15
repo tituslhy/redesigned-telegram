39e8bfcd92727027061c13e509dcf8779f2e0074
Titus Lim <tituslhy@gmail.com>
2025-07-15 13:16:37 +0800

    create mcp app

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import chainlit as cl
 import logging
 
 from langchain_core.messages import HumanMessage, AIMessage
+from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_ollama import ChatOllama
 from langgraph.graph import MessagesState, StateGraph, START, END
 from langgraph.prebuilt import create_react_agent
 
-from uuid import uuid4
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-llm = ChatOllama(model="llama3.1", temperature=0)
+llm = ChatOllama(model="qwen2.5", temperature=0)
 
 def setup_default() -> StateGraph:
     def invoke_llm(state: MessagesState):
@@ -29,6 +28,7 @@ async def on_chat_start():
     cl.user_session.set("default_agent", default_agent)
     cl.user_session.set("agent", None)
     cl.user_session.set("history", [])
+    cl.user_session.set("mcp_tools", {})
 
 @cl.on_message
 async def on_message(message: cl.Message):
@@ -36,12 +36,13 @@ async def on_message(message: cl.Message):
     history = cl.user_session.get("history")
     history.append(HumanMessage(content=message.content))
     agent = cl.user_session.get("agent")
+    logger.info("Using agent with tools")
     if agent is None:
         agent = cl.user_session.get("default_agent")
-    
+        logger.info("Using default agent")
     reply = cl.Message(content="")
     
-    for msg, metadata in agent.stream(
+    async for msg, _ in agent.astream(
         {"messages": history},
         stream_mode="messages",
         config = config
@@ -55,4 +56,54 @@ async def on_message(message: cl.Message):
     
     await reply.send()
     
+@cl.on_mcp_connect
+async def on_mcp_connect(connection: cl.mcp.SseMcpConnection | cl.mcp.StdioMcpConnection):
+    try:
+        logger.info("Connecting to MCP...")
+        mcp_tools = cl.user_session.get("mcp_tools")
+        if isinstance(connection.clientType, cl.mcp.StdioMcpConnection):
+            logger.info("Detected Stdio MCP connection")
+            connection_details = {
+                "args": [connection.command],
+                "transport": "stdio"
+            }
+        else:
+            logger.info("Detected SSE MCP connection")
+            connection_details = {
+                "url": connection.url,
+                "transport": "sse"
+            }
+        logger.info(f"Connection details: {connection_details}")
+        
+        mcp_tools[connection.name] = connection_details
+        cl.user_session.set("mcp_tools", mcp_tools)
+        
+        mcp_client = MultiServerMCPClient(mcp_tools)
+        tools = await mcp_client.get_tools()
+        agent = create_react_agent(llm, tools)
+        cl.user_session.set("agent", agent)
+        
+        logger.info(f"Connected to MCP. Connection name: {connection.name} | Connection url: {connection.url} | Connection type: {connection.clientType}")
+        await cl.Message(f"Connected to MCP server: {connection.name} on {connection.url}", type="assistant_message").send()
+        
+    except Exception as e:
+        await cl.Message(f"Error conecting to tools from MCP server: {str(e)}", type="assistant_message").send()
+
+@cl.on_mcp_disconnect
+async def on_mcp_disconnect(name: str):
+    mcp_tools = cl.user_session.get("mcp_tools", {})
+    if name in mcp_tools:
+        del mcp_tools[name]
+        logger.info(f"Disconnected from MCP server: {name}")
     
+    if len(mcp_tools) >0:
+        mcp_client = MultiServerMCPClient(mcp_tools)
+        tools = await mcp_client.get_tools()
+        agent = create_react_agent(llm, tools)
+        cl.user_session.set("agent", agent)
+    
+    else:
+        cl.user_session.set("agent", None)
+        logger.info("No more MCP servers connected, resetting agent to None")
+    
+    await cl.Message(f"Disconnected from MCP server: {name}", type="assistant_message").send()
--- tools/weather_tool.py
+++ tools/weather_tool.py
@@ -8,7 +8,7 @@ mcp = FastMCP(name="weather_tool")
 
 @mcp.tool()
 def get_weather(location: str) -> str:
-    return f"The weather in {location} is sunny. Enjoy your day!"
+    return f"The weather in {location} is sunny."
 
 if __name__ == "__main__":
     mcp.run(transport="sse", port=3000)
